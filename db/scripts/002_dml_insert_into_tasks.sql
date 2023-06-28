4f3200a10c48721c2cd7601a24700f49a80b98ea
Stepan Bogdanov <bogstepan@gmail.com>
2023-06-28 14:33:06 +0300

    4. TODO list [#3786 #426190]

--- db/scripts/002_dml_insert_into_tasks.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tasks(description, created, done) VALUES('Task1', current_date - interval '1 month', true);
-INSERT INTO tasks(description, created, done) VALUES('Task2', current_date - interval '1 week', true);
-INSERT INTO tasks(description, created, done) VALUES('Task3', current_date, false);
