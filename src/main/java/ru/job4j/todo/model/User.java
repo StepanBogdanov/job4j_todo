7cfe4cad8643b1d3fb3defc33d91cd0d3dd7df8b
Stepan Bogdanov <bogstepan@gmail.com>
2023-07-02 13:31:00 +0300

    Добавлена модель User, класс HibernateUserRepository

---
+++ src/main/java/ru/job4j/todo/model/User.java
@@ -0,0 +1,35 @@
+package ru.job4j.todo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "todo_users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String login;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(login, user.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login);
+    }
+}
---
+++ src/main/java/ru/job4j/todo/repository/HibernateUserRepository.java
@@ -0,0 +1,102 @@
+package ru.job4j.todo.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+import ru.job4j.todo.model.User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@AllArgsConstructor
+public class HibernateUserRepository implements UserRepository {
+
+    private SessionFactory sessionFactory;
+
+    @Override
+    public Optional<User> save(User user) {
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return Optional.of(user);
+    }
+
+    @Override
+    public Optional<User> findByLoginAndPassword(String login, String password) {
+        Session session = sessionFactory.openSession();
+        Optional<User> userOptional = Optional.empty();
+        try {
+            session.beginTransaction();
+            userOptional = session.createQuery("FROM todo_users WHERE login = :login AND password = :password")
+                    .setParameter("login", login)
+                    .setParameter("password", password)
+                    .uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return userOptional;
+    }
+
+    @Override
+    public Optional<User> findUserByLogin(String login) {
+        Session session = sessionFactory.openSession();
+        Optional<User> userOptional = Optional.empty();
+        try {
+            session.beginTransaction();
+            userOptional = session.createQuery("FROM todo_users WHERE login = :login")
+                    .setParameter("login", login).uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return userOptional;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        Session session = sessionFactory.openSession();
+        int updatedStrings = 0;
+        try {
+            session.beginTransaction();
+            updatedStrings = session.createQuery("DELETE FROM todo_users WHERE id = :id")
+                    .setParameter("id", id).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return updatedStrings > 0;
+    }
+
+    @Override
+    public Collection<User> findAll() {
+        Session session = sessionFactory.openSession();
+        Collection<User> users = List.of();
+        try {
+            session.beginTransaction();
+            users = session.createQuery("FROM todo_users").list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return users;
+    }
+}
